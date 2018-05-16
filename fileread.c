942d8bfb71379e556102f1a5f68b43bea5b21410
miraelim <lovemirae94@gmail.com>
2018-05-17 00:13:35 +0900

    3

---
+++ fileread.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main( )
+{
+
+    int fd;
+
+    char buf [100];
+
+    fd = open( "log.txt", O_RDONLY );
+    if( fd == -1 )
+    {
+	printf("file open error\n");
+    }
+    else
+    {
+	read(fd, buf, sizeof(buf));
+	printf("%s\n", buf);
+	close(fd);
+    }
+
+    return 0;
+}
