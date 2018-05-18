9829765f451454e54ded94244e285f93c9e90173
miraelim <lovemirae94@gmail.com>
2018-05-18 23:38:21 +0900

    11

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 git remote add origin https://github.com/miraelim/temper-prooflogging.git
 git add .
-git commit -m "10"
+git commit -m "11"
 git push -u origin master
 git remote rm origin
 git reset
